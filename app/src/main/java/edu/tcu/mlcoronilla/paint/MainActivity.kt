7b428fb078ef755cf00be4889d70c71e4f6f6b45
mycoro <mayracoronilla9@gmail.com>
2024-11-07 13:47:17 -0600

    11-07-2024, started implementing save and gallery functions

--- app/src/main/java/edu/tcu/mlcoronilla/paint/MainActivity.kt
+++ app/src/main/java/edu/tcu/mlcoronilla/paint/MainActivity.kt
@@ -1,14 +1,23 @@
 package edu.tcu.mlcoronilla.paint
 
 import android.app.Dialog
+import android.content.ContentValues
+import android.graphics.Bitmap
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.drawToBitmap
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +33,9 @@ class MainActivity : AppCompatActivity() {
         val drawingView = findViewById<DrawingView>(R.id.drawing_view) //the instance of kt file
         setUpPalette(drawingView)
         setUpPathWidthSelector(drawingView)
+        val backgroundIv = findViewById<ImageView>(R.id.background_iv)
+        setUpBackgroundPicker(backgroundIv)
+        setUpSave()
         findViewById<ImageView>(R.id.undo_iv).setOnClickListener{
             drawingView.undoPath()
         }
@@ -32,6 +44,7 @@ class MainActivity : AppCompatActivity() {
     //to set up the colors to be able to set up the change of color
     private fun setUpPalette(drawingView: DrawingView) {
         val paletteContainer = findViewById<LinearLayout>(R.id.palette_container)
+       
 
         for (i in 0 until paletteContainer.childCount) {
             val colorView = paletteContainer.getChildAt(i) as ImageView
@@ -42,7 +55,9 @@ class MainActivity : AppCompatActivity() {
                 drawingView.setPathColor(color)
             }
         }
-    }private fun setUpPathWidthSelector(drawingView: DrawingView) {
+    }
+
+    private fun setUpPathWidthSelector(drawingView: DrawingView) {
 
         findViewById<ImageView>(R.id.brush_icon).setOnClickListener{
             val dialog = Dialog(this)
@@ -61,6 +76,38 @@ class MainActivity : AppCompatActivity() {
             dialog.findViewById<ImageView>(R.id.last_width)?.setOnClickListener {
                 drawingView.setPathWidth(15)
                 dialog.dismiss()
+            }
+        }
+    }
+
+    private fun setUpBackgroundPicker(backgroundIv: ImageView) {
+        val pickMedia = registerForActivityResult(PickVisualMedia()) {
+            it?.let{Glide.with(this).load(it).into(backgroundIv)}
+        }
+        //use a listener to launch the sheet with the gallery icon
+        findViewById<ImageView>(R.id.gallery_iv).setOnClickListener { pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly)) }
+
+
+    }
+
+    private fun setUpSave() {
+        findViewById<FrameLayout>(R.id.drawing_fl).post {
+            val bitmap =
+                findViewById<FrameLayout>(R.id.drawing_fl).drawToBitmap() //generates the image
+
+            val values = ContentValues().apply { // to generate the uri
+                put(
+                    MediaStore.MediaColumns.DISPLAY_NAME,
+                    System.currentTimeMillis().toString() + ".jpeg"
+                )
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+            }
+
+            val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            uri?.let { contentResolver.openOutputStream(uri).use { image ->
+                    image?.let { bitmap.compress(Bitmap.CompressFormat.JPEG, 90, image) } //to store the image at URI
+                }
             }
         }
     }
